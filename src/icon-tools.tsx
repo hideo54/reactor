c408c4cb5c86de50796281ff2259fc2b3656f0e9
Hideo Yasumoto <hideo54@hideo54.com>
2021-12-03 09:06:00 +0900

    Adopt better structure

--- src/icon-tools.tsx
+++ src/icon-tools.tsx
@@ -1,6 +1,5 @@
 import React, { ComponentProps } from 'react';
 import Link, { LinkProps } from 'next/link';
-import styled from 'styled-components';
 import type { StyledIcon } from '@styled-icons/styled-icon';
 
 export const IconSpan: React.FC<{
@@ -89,68 +88,3 @@ export const IconNextLink: React.FC<
     );
 };
 
-const describeBackgroundGradient = (props: {
-    color: string;
-    backgroundColor?: string;
-    value: number;
-    min: number;
-    max: number;
-}) => {
-    const valuePercent = 100 * (props.value - props.min) / (props.max - props.min);
-    const gradientElements = [
-        'to right',
-        `${props.color} ${valuePercent}%`,
-        `${props.backgroundColor || 'lightgray'} 0% 100%`,
-    ];
-    return `linear-gradient(${gradientElements.join(', ')})`;
-};
-
-export const ColorfulSlider = styled.input.attrs({
-    type: 'range',
-})<{
-    color: string;
-    backgroundColor?: string;
-    value: number;
-    min: number;
-    max: number;
-}>`
-    width: 100%;
-    margin-top: 24px;
-    outline: none;
-    -webkit-appearance: none;
-    background-color: transparent;
-    &::-moz-range-track {
-        background-color: ${props => props.backgroundColor || 'lightgray'};
-        height: 8px;
-        border-radius: 4px;
-    }
-    &::-moz-range-progress {
-        background-color: ${props => props.color};
-        height: 8px;
-        border-radius: 4px;
-    }
-    &::-moz-range-thumb {
-        background-color: ${props => props.color};
-        width: 24px;
-        height: 24px;
-        border: none;
-        border-radius: 12px;
-        cursor: pointer;
-    }
-    &::-webkit-slider-runnable-track {
-        -webkit-appearance: none;
-        width: 100%;
-        height: 8px;
-        border-radius: 4px;
-        background: ${describeBackgroundGradient};
-    }
-    &::-webkit-slider-thumb {
-        -webkit-appearance: none;
-        background-color: ${props => props.color};
-        margin-top: -8px;
-        width: 24px;
-        height: 24px;
-        border-radius: 12px;
-        cursor: pointer;
-    }
-`;
---
+++ src/index.ts
@@ -0,0 +1,2 @@
+export * from './icon-tools';
+export * from './slider';
